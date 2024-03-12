5192f609359b895e10c525381f8985d8d0209345
Krishna Srivastava <7824krishna@gmail.com>
2024-03-12 21:51:53 +0530

    Update Home.jsx

--- client/src/screen/Home.jsx
+++ client/src/screen/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import NavBar from "../components/home/NavBar";
+import NavBar from "../components/Home/NavBar";
 import Sidebar from '../components/Home/Sidebar'
 
 const Home = () => {
   return (
     <div className="flex">
       <Sidebar />
+      <div className="flex flex-col w-[80%]">
       <NavBar />
+      </div>
 
     </div>
   );
